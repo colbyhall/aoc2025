8b3ebc456b3d85614980e1290b27d7652897760b
Colby <me@cobeh.com>
2025-12-01 13:35:27 -0800

    Day 1 Part 1 and Part 2

--- src/day1.zig
+++ src/day1.zig
@@ -6,7 +6,8 @@ const input = @embedFile("day1_input.txt");
 
 pub fn main() !void {
     var dial: i64 = 50;
-    var result: i64 = 0;
+    var part1: i64 = 0;
+    var part2: i64 = 0;
     var working: [:0]const u8 = input[0..];
     while (true) {
         const line = blk: {
@@ -21,35 +22,33 @@ pub fn main() !void {
             }
         };
 
-        const amount = try fmt.parseInt(usize, line[1..], 10);
+        var amount = try fmt.parseInt(i64, line[1..], 10);
         const first = line[0];
-
         switch (first) {
-            'L' => {
-                for (0..amount) |_| {
-                    dial -= 1;
-                    const actual = @mod(dial, 100);
-                    if (actual == 0) {
-                        result += 1;
-                    }
-                }
-            },
-            'R' => {
-                for (0..amount) |_| {
-                    dial += 1;
-                    const actual = @mod(dial, 100);
-                    if (actual == 0) {
-                        result += 1;
-                    }
-                }
-            },
+            'L' => amount = -amount,
+            'R' => {},
             else => unreachable,
         }
+        const old = dial;
+        dial += amount;
+        dial = @mod(dial, 100);
+        if (dial == 0) {
+            part1 += 1;
+        }
+
+        var turns: i64 = @intCast(@abs(@divTrunc(amount, 100)));
+        if (old != 0) {
+            const rem = @rem(amount, 100) + old;
+            if (rem <= 0 or rem >= 100) {
+                turns += 1;
+            }
+        }
+        part2 += turns;
 
         if (working.len == 0) {
             break;
         }
     }
 
-    std.debug.print("Result: {}\n", .{result});
+    std.debug.print("Part1 Result: {}\nPart2 Result: {}\n", .{ part1, part2 });
 }
