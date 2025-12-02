2e21befa2f68ca1a9c6aa8bd22057a9d3473cd98
Colby <me@cobeh.com>
2025-12-02 09:04:09 -0800

    Printing out input

--- src/day2.zig
+++ src/day2.zig
@@ -1,7 +1,20 @@
 const std = @import("std");
+const mem = std.mem;
 
 const input = @embedFile("day2_input.txt");
 
 pub fn main() void {
     std.debug.print("{s}\n", .{input});
+
+    var working: [:0]const u8 = input[0..];
+    while (working.len > 0) {
+        const dash = mem.indexOf(u8, working, "-") orelse unreachable;
+        const a = working[0..dash];
+        working = working[dash + 1 ..];
+        const comma = mem.indexOf(u8, working, ",") orelse working.len - 1;
+        const b = working[0..comma];
+        working = working[comma + 1 ..];
+
+        std.debug.print("{s}-{s}\n", .{ a, b });
+    }
 }
