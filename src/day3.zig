b277f87c7ec6eb890aa76d2839b25a3518a0ebdc
Colby Hall <me@cobeh.com>
2025-12-03 19:23:18 -0800

    Day2 Part2

--- src/day3.zig
+++ src/day3.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const math = std.math;
 const bench = @import("bench.zig").bench;
 
 const input = @embedFile("day3_input.txt");
@@ -36,6 +37,51 @@ fn evalPart1() !u32 {
     return total;
 }
 
+fn evalPart2() !u64 {
+    var total: u64 = 0;
+    var start: usize = 0;
+    for (0..input.len) |outer| {
+        if (input[outer] != '\n') {
+            continue;
+        }
+
+        const current = input[start..outer];
+        const number_size = 12;
+        var number: [number_size]u8 = undefined;
+
+        var last_largest_pos: usize = 0;
+        for (0..number_size) |digit| {
+            const end_delta = number_size - digit - 1;
+
+            var largest: u8 = 0;
+            inner: for (last_largest_pos..current.len - end_delta) |index| {
+                const c = current[index];
+                const value = c - '0';
+
+                if (value > largest) {
+                    largest = value;
+                    last_largest_pos = index + 1;
+
+                    if (value == 9) {
+                        break :inner;
+                    }
+                }
+            }
+            number[digit] = largest;
+        }
+
+        var joltage: u64 = 0;
+        for (0..number_size) |index| {
+            const exp = number_size - index - 1;
+            joltage += number[index] * try math.powi(u64, 10, @intCast(exp));
+        }
+        total += joltage;
+        start = outer + 1;
+    }
+    return total;
+}
+
 pub fn main() !void {
     try bench("Part1", evalPart1);
+    try bench("Part2", evalPart2);
 }
